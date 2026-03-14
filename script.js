f0f7200b6713c446f856a06a33e89d791571abf7
Odinop21 <147322280+Odinop21@users.noreply.github.com>
2026-03-14 23:36:51 +0530

    Add files via upload
    start of cinematch

---
+++ script.js
@@ -0,0 +1,13 @@
+const movie = {
+  title: "Interstellar",
+  director: "Christopher Nolan",
+  genre: "Science Fiction",
+  rating: 8.6,
+  year: 2014
+};
+console.log(movie.title);
+console.log(movie.director);
+console.log(movie.genre);
+console.log(movie.rating);
+console.log(movie.year);
+
